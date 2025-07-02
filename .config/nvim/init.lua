2a7284bfa4b440f1885b5f1f780c41c945e6d21e
Erik Ekblad <oakl33f123@gmail.com>
2025-07-02 19:47:38 +0200

    added nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,2 @@
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.g.mapleader = " "
-vim.o.number = true;
-
--- keymaps
-
-vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-z>', '<C-o>u', { noremap = true, silent = true })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+require("config.options")
+require("config.lazy")
