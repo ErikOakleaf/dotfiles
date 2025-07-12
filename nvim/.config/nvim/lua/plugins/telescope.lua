e76668619f9c06bb754cd06bfc5437dbd8125f28
Erik Ekblad <oakl33f123@gmail.com>
2025-07-12 21:50:47 +0200

    telescope changes

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,17 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = { "zig%-out/", "zig%-out/.*" },
+            },
+        })
+
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<C-r>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<C-e>', builtin.diagnostics, {})
     end
 }
