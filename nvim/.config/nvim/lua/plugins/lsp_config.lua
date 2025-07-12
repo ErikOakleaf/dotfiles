4ae3ae5defb6ddde466cff3e6190bfe2d60a19b8
Erik Ekblad <oakl33f123@gmail.com>
2025-07-12 21:34:15 +0200

    configed hyprpaper and added shortcuts for errors in nvim

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -74,6 +74,7 @@ return {
             vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
         end,
     },
 }
