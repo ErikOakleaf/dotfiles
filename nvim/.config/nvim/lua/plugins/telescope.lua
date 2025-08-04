6ee33bc068a2e7ca53b6bfb76059a623556077d2
Erik Ekblad <oakl33f123@gmail.com>
2025-08-04 14:52:05 +0200

    changed hyprland

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     config = function()
         require('telescope').setup({
             defaults = {
-                file_ignore_patterns = { "zig%-out/", "zig%-out/.*" },
+                file_ignore_patterns = { "zig%-out/", "zig%-out/.*", "^main$" },
 
             },
         })
