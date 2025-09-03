991421a55c4b443713a73011b91d2884d80a72a5
Erik Ekblad <oakl33f123@gmail.com>
2025-09-03 21:02:34 +0200

    made one monitor setup

---
+++ hypr/.config/hypr/scripts/workspace-wallpaper.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+WP1="$HOME/.config/backgrounds/wallhaven-1.png"
+WP3="$HOME/.config/backgrounds/wallhaven-3.png"
+
+set_wallpaper() {
+    local wp=$1
+    hyprctl hyprpaper wallpaper ",$wp"
+}
+
+socat - UNIX-CONNECT:"$XDG_RUNTIME_DIR"/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | \
+while read -r event; do
+    # Filter only workspace events
+    if [[ "$event" =~ "workspace" ]]; then
+        ws=$(hyprctl activeworkspace -j | jq -r '.id')
+        if [[ "$ws" == "1" || "$ws" == "2" ]]; then
+            set_wallpaper "$WP1"
+        else
+            set_wallpaper "$WP3"
+        fi
+    fi
+done
+
