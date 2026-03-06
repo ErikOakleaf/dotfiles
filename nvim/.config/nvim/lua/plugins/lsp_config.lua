e09c3e8aa49d4628fe227e762f3a9853d662f8ad
Erik Ekblad <oakl33f123@gmail.com>
2026-03-06 15:45:57 +0100

    changed nvim config to not use none-ls it was causing problems with csharpier but now the new config works

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -1,89 +1,53 @@
 return {
     {
-        "mason-org/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    -- lsps
-                    "clangd",
-                    "zls",
-                    "ts_ls",
-                    "html",
-                    "cssls",
-                    "pyright",
-                    "lua_ls",
-                    "omnisharp"
-                },
-            })
-        end,
-    },
-    {
-        "jay-babu/mason-null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "nvimtools/none-ls.nvim",
-        },
-        config = function()
-            require("mason-null-ls").setup({
-                ensure_installed = {
-                    -- formatting / linting
-                    "prettier",
-                    "eslint_d",
-                    "black",
-                    "ruff",
-                    "stylua",
-                },
-            })
-        end,
-    },
-    {
         "jay-babu/mason-nvim-dap.nvim",
-        config = function()
-            require("mason-nvim-dap").setup({
-                ensure_installed = {
-                    "codelldb", -- C/C++/Rust
-                },
-                automatic_setup = true,
-            })
-        end,
+        opts = {
+            ensure_installed = { "codelldb" }, -- C/C++/Rust
+            automatic_setup = true,
+        },
     },
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            {
+                "mason-org/mason-lspconfig.nvim",
+                opts = {
+                    ensure_installed = {
+                        "clangd",
+                        "zls",
+                        "ts_ls",
+                        "html",
+                        "cssls",
+                        "pyright",
+                        "lua_ls",
+                        "omnisharp",
+                    },
+                },
+            },
+            "hrsh7th/cmp-nvim-lsp",
+        },
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            vim.lsp.config.clangd = { capabilities = capabilities }
-            vim.lsp.enable('clangd')
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            vim.lsp.config.zls = { capabilities = capabilities }
-            vim.lsp.enable('zls')
+            local servers = {
+                "clangd",
+                "zls",
+                "ts_ls",
+                "html",
+                "cssls",
+                "pyright",
+                "lua_ls",
+                "omnisharp",
+            }
 
-            vim.lsp.config.ts_ls = { capabilities = capabilities }
-            vim.lsp.enable('ts_ls')
+            for _, server in ipairs(servers) do
+                vim.lsp.config(server, {
+                    capabilities = capabilities,
+                })
 
-            vim.lsp.config.html = { capabilities = capabilities }
-            vim.lsp.enable('html')
-
-            vim.lsp.config.cssls = { capabilities = capabilities }
-            vim.lsp.enable('cssls')
-
-            vim.lsp.config.pyright = { capabilities = capabilities }
-            vim.lsp.enable('pyright')
-
-            vim.lsp.config.lua_ls = { capabilities = capabilities }
-            vim.lsp.enable('lua_ls')
-
-            vim.lsp.config.lua_ls = { capabilities = capabilities }
-            vim.lsp.enable('lua_ls')
-
-            vim.lsp.config.omnisharp = { capabilities = capabilities }
-            vim.lsp.enable('omnisharp')
+                vim.lsp.enable(server)
+            end
 
             -- keymaps
             vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- LSPs
+					"clangd",
+					"zls",
+					"ts_ls",
+					"html",
+					"cssls",
+					"pyright",
+					"lua_ls",
+					"omnisharp",
+					-- Formatters
+					"prettier",
+					"black",
+					"stylua",
+					"csharpier",
+					-- Linters
+					"eslint_d",
+					"ruff",
+					-- Debuggers
+					"codelldb",
+				},
+			})
+		end,
+	},
+}
