8d4e8a05cfe3b846d2caf545c3a7892f1de287d9
Erik Ekblad <oakl33f123@gmail.com>
2026-03-13 13:39:49 +0100

    fixed nvim catppuccin

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,22 +1,22 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            transparent_background = true,
-            float = {
-                transparent = true,
-            },
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+			float = {
+				transparent = true,
+			},
 
-            custom_highlights = function(colors)
-                return {
-                    LineNr = { fg = colors.text },
-                    LineNrRelative = { fg = colors.text },
-                }
-            end
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
-
+			custom_highlights = function(colors)
+				return {
+					LineNr = { fg = colors.text },
+					LineNrRelative = { fg = colors.text },
+				}
+			end,
+		})
+        vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
