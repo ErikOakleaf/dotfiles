e7a29c699c002492e056d361a3c905691684a32b
Erik Ekblad <oakl33f123@gmail.com>
2026-04-26 19:24:28 +0200

    changed some vim key commands

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,10 +1,11 @@
 local cmp = require("cmp")
+local ls = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
+			ls.lsp_expand(args.body)
 		end,
 	},
 	window = {
@@ -12,16 +13,36 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
-		-- custom navigation keys
-		["<C-u>"] = cmp.mapping.select_next_item(),
-		["<C-i>"] = cmp.mapping.select_prev_item(),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if ls.expand_or_jumpable() then
+				ls.expand_or_jump()
+			elseif cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		-- Context based shift tab navigation
+		-- If menu open -> Previous item
+		-- If in snippet  -> Previous argument
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if ls.jumpable(-1) then
+				ls.jump(-1)
+			elseif cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	}),
+
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
